851800b053e546ac5b355c6b5c1ee24f724cc4b9
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-08-30 17:00:09 -0500

    adds classes and modules

---
+++ lib/game.rb
@@ -0,0 +1,41 @@
+class Game
+    attr_accessor :board, :player_1, :player_2
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ]
+    def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+        @player_1 = player_1
+        @player_2 = player_2
+        @board = board
+    end
+    def current_player
+        self.board.turn_count % 2 == 0 ? player_1 : player_2
+    end
+    def won?
+        WIN_COMBINATIONS.detect do |win|
+            self.board.cells[win[0]] == self.board.cells[win[1]] && self.board.cells[win[1]] == self.board.cells[win[2]] &&
+            (self.board.cells[win[0]] == "X" || self.board.cells[win[0]] == "O")
+        end
+    end
+    def draw?
+        self.board.full? && !won?
+    end
+    def over?
+        won? || draw?
+    end
+    def winner
+        if win = won?
+            @winner = self.board.cells[win.first]
+        end
+    end 
+end
+
+
+        
---
+++ lib/players/human.rb
@@ -0,0 +1,8 @@
+class Players
+    class Human < Player
+        def move(arg)
+            puts 'Please enter "X" or "O" to select token.'
+            gets.strip
+        end
+    end
+end
